285230c7e7da0ded0f494821c5c3c488685500e5
mohammadashrafulislam1 <ixdell41@gmail.com>
2024-10-19 10:25:47 +0600

    added router for client

---
+++ backend/Router/client.js
@@ -0,0 +1,12 @@
+import express from "express";
+import { addClient, updateClient } from "../Controllers/ClientController.js";
+import { upload } from "../Middleware/multer.js";
+
+export const clientRouter = express.Router();
+
+
+// Route to add a new client
+clientRouter.post('/', upload.single('image'), addClient);
+
+// Route to update an existing client
+clientRouter.put('/:id/:clientId', upload.single('image'), updateClient);
