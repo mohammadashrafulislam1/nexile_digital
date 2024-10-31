c4166f99200ab63c5a10770a37b11885b1c3c567
mohammadashrafulislam1 <ixdell41@gmail.com>
2024-10-31 11:15:16 +0600

    addding deleting and updating blogs is working

--- dashboard/src/Pages/AddBlog.jsx
+++ dashboard/src/Pages/AddBlog.jsx
@@ -1,20 +1,25 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import axios from 'axios';
 import { Link, useLocation, useNavigate } from 'react-router-dom';
-import { ToastContainer } from 'react-toastify';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import { endPoint } from '../Components/ForAll/ForAll';
+import { TagsInput } from 'react-tag-input-component';
+import Swal from 'sweetalert2';
 
 const AddBlog = () => {
     const location = useLocation();
     const navigate = useNavigate();
     const blogToEdit = location.state?.blog || null;
+    console.log(blogToEdit)
 
     const [title, setTitle] = useState(blogToEdit?.title || '');
     const [category, setCategory] = useState(blogToEdit?.category || '');
     const [description, setDescription] = useState(blogToEdit?.description || '');
     const [metaTitle, setMetaTitle] = useState(blogToEdit?.metaTitle || '');
     const [metaDescription, setMetaDescription] = useState(blogToEdit?.metaDescription || '');
-    const [tags, setTags] = useState(blogToEdit?.tags?.join(', ') || ''); // Joining tags as a comma-separated string for input
-    const [image, setImage] = useState(null);
+    const [tags, setTags] = useState(blogToEdit?.tags || []); // Initialize as an array for `TagsInput`
+    const [image, setImage] =useState(blogToEdit?.image ||null);
 
     // Handle image file change
     const handleImageChange = (e) => {
@@ -31,73 +36,99 @@ const AddBlog = () => {
         formData.append('description', description);
         formData.append('metaTitle', metaTitle);
         formData.append('metaDescription', metaDescription);
-        formData.append('tags', tags.split(',').map(tag => tag.trim())); // Splitting back into an array
+        formData.append('tags', JSON.stringify(tags)); // Convert tags array to JSON
         if (image) formData.append('image', image);
 
+          // Log formData entries
+    for (const [key, value] of formData.entries()) {
+        console.log(`${key}:`, value);
+    }
+    
         try {
             if (blogToEdit) {
-                // Update existing blog post
-                await axios.put(`/api/blogs/${blogToEdit._id}`, formData);
+                await axios.put(`${endPoint}/blog/${blogToEdit._id}`, formData);
+                toast.success('Blog post updated successfully!');
             } else {
-                // Add new blog post
-                await axios.post('/api/blogs', formData);
+                await axios.post(`${endPoint}/blog`, formData);
+                toast.success('Blog post created successfully!');
             }
+            Swal.fire({
+                position: "top-end",
+                icon: "success",
+                title: `${blogToEdit? "Blog updated successfully!" :"Blog added successfully!" }`,
+                showConfirmButton: false,
+                timer: 1500
+              });
             navigate('/blogs');
         } catch (error) {
             console.error('Error submitting form', error);
+            toast.error('Failed to submit the form. Please try again.');
         }
     };
 
     return (
         <div className="container mx-auto p-6">
             <div className="breadcrumbs text-sm lg:w-1/2 md:w-[80%] w-[96%] mx-auto">
-        <ul className="my-6">
-          <li>
-            <Link to="/" className="inline-flex items-center gap-2 text-blue-600 hover:underline">
-            <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                className="h-4 w-4 stroke-current">
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M3 7v10a2 2 0 002 2h14a2 2 0 002-2V9a2 2 0 00-2-2h-6l-2-2H5a2 2 0 00-2 2z"></path>
-              </svg>
-              Home
-            </Link>
-          </li>
-          <li>
-            <span className="inline-flex items-center gap-2 text-gray-700"><svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                className="h-4 w-4 stroke-current">
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M9 13h6m-3-3v6m5 5H7a2 2 0 01-2-2V5a2 2 0 012-2h5.586a1 1 0 01.707.293l5.414 5.414a1 1 0 01.293.707V19a2 2 0 01-2 2z"></path>
-              </svg>
-              Services</span>
-          </li>
-        </ul>
-        <ToastContainer />
-      </div>
-            <form onSubmit={handleSubmit} className="lg:w-1/2 md:w-[80%] w-[96%] mx-auto bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4 space-y-4">
-                <h2 className="text-2xl font-bold text-center mb-6">{blogToEdit ? 'Edit Blog Post' : 'Add New Blog Post'}</h2>
+                <ul className="my-6">
+                    <li>
+                        <Link to="/" className="inline-flex items-center gap-2 text-blue-600 hover:underline">
+                            <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                fill="none"
+                                viewBox="0 0 24 24"
+                                className="h-4 w-4 stroke-current"
+                            >
+                                <path
+                                    strokeLinecap="round"
+                                    strokeLinejoin="round"
+                                    strokeWidth="2"
+                                    d="M3 7v10a2 2 0 002 2h14a2 2 0 002-2V9a2 2 0 00-2-2h-6l-2-2H5a2 2 0 00-2 2z"
+                                ></path>
+                            </svg>
+                            Home
+                        </Link>
+                    </li>
+                    <li>
+                        <span className="inline-flex items-center gap-2 text-gray-700">
+                            <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                fill="none"
+                                viewBox="0 0 24 24"
+                                className="h-4 w-4 stroke-current"
+                            >
+                                <path
+                                    strokeLinecap="round"
+                                    strokeLinejoin="round"
+                                    strokeWidth="2"
+                                    d="M9 13h6m-3-3v6m5 5H7a2 2 0 01-2-2V5a2 2 0 012-2h5.586a1 1 0 01.707.293l5.414 5.414a1 1 0 01.293.707V19a2 2 0 01-2 2z"
+                                ></path>
+                            </svg>
+                            Services
+                        </span>
+                    </li>
+                </ul>
+                <ToastContainer />
+            </div>
+            
+            <Link to="/blogs"><button className="btn btn-sm mb-2 flex justify-center items-center mx-auto bg-black text-white">Blogs</button></Link>
+            <form
+                onSubmit={handleSubmit}
+                className="lg:w-1/2 md:w-[80%] w-[96%] mx-auto bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4 space-y-4"
+            >
+                <h2 className="text-2xl font-bold text-center mb-6">
+                    {blogToEdit ? 'Edit Blog Post' : 'Add New Blog Post'}
+                </h2>
 
                 <div className="form-control">
                     <label className="label">
                         <span className="label-text">Title</span>
                     </label>
-                    <input 
-                        type="text" 
+                    <input
+                        type="text"
                         className="input input-bordered w-full"
-                        value={title} 
-                        onChange={(e) => setTitle(e.target.value)} 
-                        required 
+                        value={title}
+                        onChange={(e) => setTitle(e.target.value)}
+                        required
                     />
                 </div>
 
@@ -105,12 +136,12 @@ const AddBlog = () => {
                     <label className="label">
                         <span className="label-text">Category</span>
                     </label>
-                    <input 
-                        type="text" 
+                    <input
+                        type="text"
                         className="input input-bordered w-full"
-                        value={category} 
-                        onChange={(e) => setCategory(e.target.value)} 
-                        required 
+                        value={category}
+                        onChange={(e) => setCategory(e.target.value)}
+                        required
                     />
                 </div>
 
@@ -118,11 +149,11 @@ const AddBlog = () => {
                     <label className="label">
                         <span className="label-text">Description</span>
                     </label>
-                    <textarea 
-                        className="textarea textarea-bordered w-full" 
-                        value={description} 
-                        onChange={(e) => setDescription(e.target.value)} 
-                        required 
+                    <textarea
+                        className="textarea textarea-bordered w-full"
+                        value={description}
+                        onChange={(e) => setDescription(e.target.value)}
+                        required
                     />
                 </div>
 
@@ -130,11 +161,11 @@ const AddBlog = () => {
                     <label className="label">
                         <span className="label-text">Meta Title</span>
                     </label>
-                    <input 
-                        type="text" 
-                        className="input input-bordered w-full" 
-                        value={metaTitle} 
-                        onChange={(e) => setMetaTitle(e.target.value)} 
+                    <input
+                        type="text"
+                        className="input input-bordered w-full"
+                        value={metaTitle}
+                        onChange={(e) => setMetaTitle(e.target.value)}
                     />
                 </div>
 
@@ -142,22 +173,23 @@ const AddBlog = () => {
                     <label className="label">
                         <span className="label-text">Meta Description</span>
                     </label>
-                    <textarea 
-                        className="textarea textarea-bordered w-full" 
-                        value={metaDescription} 
-                        onChange={(e) => setMetaDescription(e.target.value)} 
+                    <textarea
+                        className="textarea textarea-bordered w-full"
+                        value={metaDescription}
+                        onChange={(e) => setMetaDescription(e.target.value)}
                     />
                 </div>
 
                 <div className="form-control">
                     <label className="label">
-                        <span className="label-text">Tags (comma-separated)</span>
+                        <span className="label-text">Tags</span>
                     </label>
-                    <input 
-                        type="text" 
-                        className="input input-bordered w-full" 
-                        value={tags} 
-                        onChange={(e) => setTags(e.target.value)} 
+                    <TagsInput
+                        value={tags}
+                        onChange={setTags}
+                        name="tags"
+                        classNames="p-4"
+                        placeHolder="Add tags..."
                     />
                 </div>
 
@@ -165,15 +197,18 @@ const AddBlog = () => {
                     <label className="label">
                         <span className="label-text">Upload Image</span>
                     </label>
-                    <input 
-                        type="file" 
-                        className="file-input file-input-bordered w-full" 
-                        onChange={handleImageChange} 
+                    <input
+                        type="file"
+                        className="file-input file-input-bordered w-full"
+                        onChange={handleImageChange}
                     />
+                   {
+                    image &&  <img src={image} alt={title}  className='w-32 h-32 my-4 object-cover rounded-xl'/>
+                   }
                 </div>
 
                 <div className="form-control mt-6">
-                    <button type="submit" className="btn btn-primary w-full">
+                    <button type="submit" className="btn bg-black text-white w-full">
                         {blogToEdit ? 'Update Blog Post' : 'Create Blog Post'}
                     </button>
                 </div>
--- dashboard/src/Pages/Blogs.jsx
+++ dashboard/src/Pages/Blogs.jsx
@@ -1,10 +1,164 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { BsPencilSquare } from "react-icons/bs";
+import { FaTrashAlt } from "react-icons/fa";
+import { endPoint } from "../Components/ForAll/ForAll";
+import Swal from "sweetalert2";
 
-const Blogs = () =>{
-    return(
-        <div>
+const Blogs = () => {
+    const [blogs, setBlogs] = useState([]);
+    const navigate = useNavigate()
+    const [loading, setLoading] = useState(false);
+    const [searchTerm, setSearchTerm] = useState("");
 
+    useEffect(() => {
+        fetchBlog();
+    }, []);
+
+    const fetchBlog = async () => {
+        setLoading(true)
+        const response = await axios.get(`${endPoint}/blog`);
+        // Parse tags for each blog
+         const sortedProperties = response.data
+                    .sort((a, b) => new Date(b.created_at) - new Date(a.created_at))
+                    .slice(0, 8);
+        const blogsWithParsedTags = sortedProperties.map(blog => ({
+            ...blog,
+            tags: JSON.parse(blog.tags) // Parse tags string into an array
+        }));
+        setBlogs(blogsWithParsedTags);
+        setLoading(false)
+    };
+    const handleEditBlog = (blog) => {
+        // Handle edit work functionality
+    navigate("/addblog", { state: { blog } });
+    };
+
+    // Filter blogs based on search term
+    const filteredBlogs = blogs.filter((blog) =>
+        blog.title.toLowerCase().includes(searchTerm.toLowerCase())
+    );
+    const handleDeleteBlog = async (blog) => {
+        const confirmDelete = await Swal.fire({
+            title: "Are you sure?",
+            text: "This action cannot be undone.",
+            icon: "warning",
+            showCancelButton: true,
+            confirmButtonColor: "#d33",
+            cancelButtonColor: "#3085d6",
+            confirmButtonText: "Yes, delete it!",
+        });
+
+        if (confirmDelete.isConfirmed) {
+            try {
+                await axios.delete(`${endPoint}/blog/${blog._id}`);
+                Swal.fire({
+                    position: "top-end",
+                    icon: "success",
+                    title: `Blog ${blog?.title} deleted successfully!`,
+                    showConfirmButton: false,
+                    timer: 1500,
+                });
+                fetchBlog(); // Refresh the blogs list
+            } catch (error) {
+                console.error("Error deleting blog:", error);
+                Swal.fire({
+                    icon: "error",
+                    title: "Oops...",
+                    text: "Something went wrong while deleting the blog!",
+                });
+            }
+        }
+    };
+    if (loading) {
+        return (
+          <div className="flex justify-center items-center h-screen">
+            <span className="loading loading-bars loading-lg"></span>
+          </div>
+        );
+      }
+    return (
+        <div className="p-4 overflow-hidden">
+            <h1 className="md:text-3xl text-xl font-semibold mb-4 text-center">Manage Blogs</h1>
+            <Link to="/addblog">
+                <button className="btn btn-sm mb-2 flex justify-center items-center mx-auto bg-black text-white">
+                    Add Blog
+                </button>
+            </Link>
+
+            {/* Search Input */}
+            <div className="flex justify-center items-center">
+                <input
+                    type="text"
+                    placeholder="Search blogs..."
+                    className="mb-4 p-2 lg:w-1/3 md:w-1/2 w-3/4 border border-gray-300 rounded shadow"
+                    value={searchTerm}
+                    onChange={(e) => setSearchTerm(e.target.value)}
+                />
+            </div>
+
+            <div className="overflow-x-auto mr-3 !rounded-[15px]">
+                <table className="min-w-full bg-white border-gray-200 !rounded-[15px]">
+                    <thead>
+                        <tr className="bg-black text-white">
+                            <th className="py-3 px-6 text-left text-xs font-semibold uppercase tracking-wider">Image</th>
+                            <th className="py-3 px-6 text-left text-xs font-semibold uppercase tracking-wider">Title</th>
+                            <th className="py-3 px-6 text-left text-xs font-semibold uppercase tracking-wider">Category</th>
+                            <th className="py-3 px-6 text-left text-xs font-semibold uppercase tracking-wider">Tags</th>
+                            <th className="py-3 px-6 text-left text-xs font-semibold uppercase tracking-wider">Actions</th>
+                        </tr>
+                    </thead>
+                    <tbody className="!rounded-xl">
+                        {filteredBlogs.map((blog) => (
+                            <tr key={blog._id} className="border-t border-gray-200 hover:bg-gray-900 hover:text-white">
+                                <td className="py-3 px-6">
+                                    <img
+                                        className="w-20 h-20 object-cover rounded"
+                                        src={blog.image}
+                                        alt={blog.title || "Blog Image"}
+                                    />
+                                </td>
+                                <td className="py-3 px-2 text-xl font-medium">{blog.title}</td>
+                                <td className="py-3 px-2 text-sm">{blog.category}</td>
+                                <td className="py-3 px-2">
+                                    <div className="flex flex-wrap gap-1">
+                                        {blog.tags.map((tag, index) => (
+                                            <span
+                                                key={index}
+                                                className="bg-gray-300 rounded-full px-2 py-0.5 text-xs text-gray-800"
+                                            >
+                                                {tag}
+                                            </span>
+                                        ))}
+                                    </div>
+                                </td>
+                                <td className="py-3 px-6 text-center">
+                                    <div className="flex gap-2 justify-center">
+                                        <button
+                                            aria-label="Edit blog"
+                                            className="text-blue-600 hover:text-blue-800 text-lg p-2 rounded-full bg-white border border-gray-200"
+                                            onClick={() => handleEditBlog(blog)}
+                                        >
+                                            <BsPencilSquare />
+                                        </button>
+                                        <button
+                                            aria-label="Delete blog"
+                                            type="button"
+                                            className="text-red-600 hover:text-red-800 text-lg p-2 rounded-full bg-white border border-gray-200"
+                                            onClick={() => handleDeleteBlog(blog)}
+                                        >
+                                            <FaTrashAlt />
+                                        </button>
+                                    </div>
+                                </td>
+                            </tr>
+                        ))}
+                    </tbody>
+                </table>
+            </div>
         </div>
-    )
-}
+    );
+};
 
 export default Blogs;
