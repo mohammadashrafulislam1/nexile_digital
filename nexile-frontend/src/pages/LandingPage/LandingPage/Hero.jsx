f69a21520769d659ac565f3e77472a91c260ebd5
mohammadashrafulislam1 <ixdell41@gmail.com>
2024-11-08 11:22:47 +0600

    added nexile digital

--- nexile-frontend/src/pages/LandingPage/LandingPage/Hero.jsx
+++ nexile-frontend/src/pages/LandingPage/LandingPage/Hero.jsx
@@ -1,6 +1,8 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
 import { endPoint } from "../../../Components/ForAll/ForAll";
+import Rating from "react-rating";
+import { FaRegStar, FaStar } from "react-icons/fa";
 
 const Hero =()=>{
     const [header, setHeader] = useState();
@@ -44,7 +46,48 @@ const Hero =()=>{
             <div className="pt-[160px] pb-14">
              <h2 className="text-[130px] font-bold text-white text-center uppercase" style={{lineHeight:'130px', letterSpacing:'-5px'}}>{header[0]?.title}</h2>
             <p className="text-[30px] text-white font-[100] text-center">{header[0]?.description}</p>
-            <button className="bg-white py-2 px-7 rounded-md poppins-medium text-[25px] flex justify-center items-center">Let’s Get Solution!</button>
+            <div className="flex justify-center mt-10"><button className="bg-white py-2 px-7 rounded-sm poppins-medium text-[25px]">Let’s Get Solution!</button></div>
+           
+
+           <div>
+            {/* Google Review */}
+            <div className="flex items-center gap-3">
+                <div>
+                    <h6 className="poppins-regular text-white text-[13px]">Reviews on</h6>
+                    <img src="https://res.cloudinary.com/dnwmtd4p1/image/upload/v1731042430/nexile%20digital/asset/vlfkczjzdigzssifail5.webp" 
+                    alt="google logo, md ashraful islam, portfolio" className="w-[58px]" />
+                </div>
+                <div className="flex flex-col gap-0">
+                <Rating
+                    initialRating={header[0]?.googleReview}
+                    emptySymbol={<FaRegStar />}
+                    fullSymbol={<FaStar />}
+                    readonly
+                    className="text-[20px] text-[#00ECFB] mb-0"
+                  />
+                  <p className="text-white poppins-medium text-[16px] mt-[-5px]">Got {header[0]?.googleReview} stars</p>
+                </div>
+            </div>
+            {/* Nexile Review */}
+            <div className="flex items-center gap-3">
+                <div>
+                    <h6 className="poppins-regular text-white text-[13px]">Reviews on</h6>
+                    <img src="https://res.cloudinary.com/dnwmtd4p1/image/upload/v1731042430/nexile%20digital/asset/zyg5fsrl4n7oyssnvf06.webp" 
+                    alt="nexile digital logo, md ashraful islam, portfolio" className="w-[58px]" />
+                </div>
+                <div className="flex flex-col gap-0">
+                <Rating
+                    initialRating={header[0]?.nexileReview}
+                    emptySymbol={<FaRegStar />}
+                    fullSymbol={<FaStar />}
+                    readonly
+                    className="text-[20px] text-[#00ECFB] mb-0"
+                  />
+                  <p className="text-white poppins-medium text-[16px] mt-[-5px]">Got {header[0]?.nexileReview} stars</p>
+                </div>
+            </div>
+           </div>
+           
             </div>
             
         )
