a4182a3e9c84cf353eff3560baff9d8b18ccfdbb
mohammadashrafulislam1 <ixdell41@gmail.com>
2024-11-19 19:13:30 +0600

    added services section

--- nexile-frontend/src/pages/LandingPage/LandingPage/LandingPage.jsx
+++ nexile-frontend/src/pages/LandingPage/LandingPage/LandingPage.jsx
@@ -1,6 +1,7 @@
 import { Helmet } from "react-helmet";
 import Header from "../../../Components/ForAll/Header";
 import Hero from "./Hero";
+import Service from "./Services";
 
 const LandingPage = ()=>{
     return(
@@ -11,13 +12,16 @@ const LandingPage = ()=>{
     </Helmet>
 
     {/* Content */}
-    <div 
+   <div className="bg-black">
+   <div 
   className="bg-black relative" 
-  style={{
+  >
+  <div style={{
     backgroundImage: 'url(https://res.cloudinary.com/dnwmtd4p1/image/upload/v1731040743/nexile%20digital/asset/otjfpjewv5z1bubx26kr.webp)',
   }}
 >
-  {/* Background images */}
+    {/* Background images */}
+  <div className="z-0">
   <img 
     src="https://res.cloudinary.com/dnwmtd4p1/image/upload/v1731040744/nexile%20digital/asset/krjhtmdwyfeoebirihzq.webp" 
     alt="" 
@@ -26,19 +30,24 @@ const LandingPage = ()=>{
   <img 
     src="https://res.cloudinary.com/dnwmtd4p1/image/upload/v1731040743/nexile%20digital/asset/ebqjrqhwdf0puwpmup9g.webp" 
     alt="" 
-    className="absolute right-0 z-0"
+    className="absolute right-0"
   />
+  </div>
   
   {/* Content */}
   <Header className="z-30" />
   <Hero className="!z-30" />
+  
 
   {/* Black shadow at the bottom */}
   <div 
     className="absolute bottom-0 left-0 right-0 h-20 bg-gradient-to-t from-black to-transparent"
   ></div>
+  
+   </div>
+   </div>
+  <Service className="!z-60"/>
 </div>
-
     </div>
     )
 }
---
+++ nexile-frontend/src/pages/LandingPage/LandingPage/Services.jsx
@@ -0,0 +1,16 @@
+const Service = () =>{
+    return(
+    <div className="px-8">
+        <div className="flex justify-center items-center">
+        <h2 className="text-white py-20 text-[130px] uppercase font-bold underline">
+         services
+         NEXILE
+         PROVIDES</h2>
+         <p className="font-[100] text-white text-[20px]">At Nexile Digital, we provide a wide range of digital solutions designed to enhance your online presence, streamline your business processes, and help you achieve your goals. Whether you're looking to create a stunning website, boost your search engine rankings, or develop custom software, our expert team is here to help. Explore our services below to see how we can support your business.</p>
+        
+        </div>
+    </div>
+    )
+}
+
+export default Service;
