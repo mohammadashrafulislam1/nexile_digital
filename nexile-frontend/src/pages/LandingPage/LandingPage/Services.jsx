d2032a2a63314370e92bf1837c6dd4a724cada99
mohammadashrafulislam1 <ixdell41@gmail.com>
2024-11-19 23:04:39 +0600

    added

--- nexile-frontend/src/pages/LandingPage/LandingPage/Services.jsx
+++ nexile-frontend/src/pages/LandingPage/LandingPage/Services.jsx
@@ -1,14 +1,19 @@
+import { Link } from "react-router-dom";
+
 const Service = () =>{
     return(
     <div className="px-8">
-        <div className="flex justify-center items-center">
-        <h2 className="text-white py-20 text-[130px] uppercase font-bold underline">
+        <div className="flex justify-end items-center">
+        <h2 className="text-white py-20 text-[130px] uppercase font-bold underline leading-[140px]">
          services
          NEXILE
          PROVIDES</h2>
+         <div>
          <p className="font-[100] text-white text-[20px]">At Nexile Digital, we provide a wide range of digital solutions designed to enhance your online presence, streamline your business processes, and help you achieve your goals. Whether you're looking to create a stunning website, boost your search engine rankings, or develop custom software, our expert team is here to help. Explore our services below to see how we can support your business.</p>
-        
-        </div>
+<Link to='/'>
+<img src="https://res.cloudinary.com/dnwmtd4p1/image/upload/v1732024726/nexile%20digital/assets/ejj6ajpv5ykj5qdgeqmi.png" className="w-[155px] ml-10" alt="" />
+</Link>   </div> 
+       </div>
     </div>
     )
 }
