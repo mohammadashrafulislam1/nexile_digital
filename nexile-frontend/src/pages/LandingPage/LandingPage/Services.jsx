71fbacb82ec862728098d05ecb6992f116a44c7e
mohammadashrafulislam1 <ixdell41@gmail.com>
2024-12-02 20:40:31 +0600

    added nexile responsive ness

--- nexile-frontend/src/pages/LandingPage/LandingPage/Services.jsx
+++ nexile-frontend/src/pages/LandingPage/LandingPage/Services.jsx
@@ -117,36 +117,54 @@ const SkeletonLoader = () => (
                 services?.map((service) => (
                   <div
                     key={service._id}
-                    className="group grid md:grid-cols-12 h-10 hover:h-[550px] gap-3 md:h-[151px] h-[150px] justify-center items-center border-b-2 relative"
+                    className="group grid md:grid-cols-12 h-10 hover:h-[550px] md:hover:h-[151px] md:gap-3 gap-0 md:h-[151px] h-[100px] my-auto justify-between items-center border-b-2 relative"
                   >
                     {/* Service Title */}
-                    <h3 className="text-[#A8A8A8] lg:text-[46px] text-[20px] md:text-[30px] poppins-semibold md:col-span-4 group-hover:text-white relative">
+                    <h3 className="text-[#A8A8A8] lg:text-[46px] text-[20px] md:text-[30px] poppins-semibold md:col-span-4 group-hover:text-white relative md:mb-0 mb-[-70px]">
                       {service.title}
                       {/* Square Dot */}
                       <span className="hidden group-hover:inline-block bg-[#00ECFB] w-2 h-2 ml-2 rounded-sm absolute bottom-2"></span>
                     </h3>
       
                     {/* Subtitle */}
-                    <p className="text-transparent text-[20px] poppins-light md:col-span-4 px-3 group-hover:text-white transition duration-300">
+                    <p className="mb-2 md:mt-0 mt-[-100px] text-transparent group-hover:block lg:text-[20px] md:text-[16px] text-[16px] poppins-light md:col-span-4 md:px-3 group-hover:text-white transition duration-300">
                       {service?.subtitle}
                     </p>
       
                     {/* Main Service Image */}
-                    <div className="relative md:col-span-3 overflow-visible">
+                    <div className="relative md:col-span-3 mt-[-100px] md:mt-4 overflow-visible  md:block hidden">
                       <img
                         src={service?.mainServiceImage}
                         alt={`${service.title}, nexile digital`}
-                        className="hidden group-hover:block w-[300px] h-[250px] object-cover 
-                          rounded-xl md:absolute md:group-hover:translate-y-[-10px] transition duration-300 z-10 lg:top-[-120px]"
+                        className="hidden group-hover:block md:w-[300px] lg:h-[250px] md:h-[180px] w-[240px] h-[160px] object-cover 
+                          rounded-xl md:absolute md:group-hover:translate-y-[-10px] transition duration-300 z-10 lg:top-[-120px] md:top-[-80px] top-[-280px]"
                       />
                     </div>
       
                     {/* Right Arrow */}
                     <div
-                      className="text-white border border-2px md:text-[40px] text-[25px] font-[400] py-3 px-2 rounded-full md:w-16 md:h-16 w-8 h-8 flex justify-center items-center md:col-span-1 
-                      group-hover:bg-white group-hover:text-black transition duration-300"
+                      className="text-white border border-2px lg:text-[40px] mt-1 md:mt-0 md:text-[30px] text-[25px] font-[400] py-4 px-2 md:py-2 md:px-2 rounded-full md:w-16 md:h-16 w-8 h-8 flex justify-center items-center md:col-span-1 
+                      group-hover:bg-white group-hover:text-black transition duration-300  md:block hidden "
                     >
                       <BsArrowRight />
+                    </div>
+                    <div className="flex items-center justify-between md:hidden block">
+                    <div className="relative md:col-span-3 mt-[-140px] md:mt-4 overflow-visible">
+                      <img
+                        src={service?.mainServiceImage}
+                        alt={`${service.title}, nexile digital`}
+                        className="hidden group-hover:block md:w-[300px] lg:h-[250px] md:h-[180px] w-[240px] h-[160px] object-cover 
+                          rounded-xl md:absolute md:group-hover:translate-y-[-10px] transition duration-300 z-10 lg:top-[-120px] md:top-[-80px] top-[-280px]"
+                      />
+                    </div>
+      
+                    {/* Right Arrow */}
+                    <div
+                      className="text-white border border-2px lg:text-[40px] mt-[-80px] md:text-[30px] text-[25px] font-[400] py-3 px-2 md:py-2 md:px-2 rounded-full md:w-16 md:h-16 w-8 h-8 flex justify-center items-center 
+                      group-hover:bg-white group-hover:text-black transition duration-300 group-hover:mt-[-100px]"
+                    >
+                      <BsArrowRight />
+                    </div>
                     </div>
                   </div>
                 ))
