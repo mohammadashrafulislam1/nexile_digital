329b07a2a53b453be727149bf091bd0f7c1e8cdf
mohammadashrafulislam1 <ixdell41@gmail.com>
2024-12-07 10:32:34 +0600

    added how we work second

--- nexile-frontend/src/pages/LandingPage/LandingPage/HowWeWork.jsx
+++ nexile-frontend/src/pages/LandingPage/LandingPage/HowWeWork.jsx
@@ -32,9 +32,41 @@ const HowWeWork = () =>{
     return(
         <div className="px-8 lg:py-5 relative overflow-x-hidden"> <h2 className="text-white lg:py-20 pb-6 lg:text-[130px] text-[40px] uppercase font-bold underline lg:leading-[160px] leading-[40px]">
      HOW WE WORK
-              </h2>
+          </h2>
        
+        <div className="grid grid-cols-2 gap-18">
+            <div className="flex gap-2">
+                <div className="relative w-16 h-16">
+                    <div className={`bg-[#${howWeWork[0]?.color}] w-16 h-16 rounded-full opacity-50`}></div>
+                    <div className={`bg-[#${howWeWork[0]?.color}] w-10 h-10 rounded-full opacity-100 
+                    absolute top-[17%] left-[18%]`}></div>
+                    <div className={`bg-[#fff] w-6 h-6 rounded-full opacity-100 absolute top-[29%] left-[31%]`}></div>
+                </div>
+                <div className="w-[45%]">
+                <h6 className="text-white text-[20px] poppins-medium underline">{howWeWork[0]?.title}</h6>
+                <h6 className="text-white text-[16px] poppins-light">{howWeWork[0]?.description}</h6>
+                </div>
+                <div className="w-[260px] h-[180px] rounded-2xl">
+                    <img src={howWeWork[0]?.thumbnail} alt={howWeWork[0]?.title} className="w-[260px] h-[180px] rounded-2xl"/>
+                </div>
+            </div>
 
+            <div className="flex gap-2">
+                <div className="w-[260px] h-[180px] rounded-2xl">
+                    <img src={howWeWork[3]?.thumbnail} alt={howWeWork[3]?.title} className="w-[260px] h-[180px] rounded-2xl"/>
+                </div>
+                <div className="relative w-16 h-16">
+                    <div className={`bg-[#${howWeWork[3]?.color}] w-16 h-16 rounded-full opacity-50`}></div>
+                    <div className={`bg-[#${howWeWork[3]?.color}] w-10 h-10 rounded-full opacity-100 
+                    absolute top-[17%] left-[18%]`}></div>
+                    <div className={`bg-[#fff] w-6 h-6 rounded-full opacity-100 absolute top-[29%] left-[31%]`}></div>
+                </div>
+                <div className="w-[45%]">
+                <h6 className="text-white text-[20px] poppins-medium underline">{howWeWork[3]?.title}</h6>
+                <h6 className="text-white text-[16px] poppins-light">{howWeWork[3]?.description}</h6>
+                </div>
+            </div>
+        </div>
 
 
         </div>
