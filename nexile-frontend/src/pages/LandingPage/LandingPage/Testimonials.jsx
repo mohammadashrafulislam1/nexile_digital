6b10b46aeadcd25fc64e101033fdc767ce7a123a
mohammadashrafulislam1 <ixdell41@gmail.com>
2024-12-21 05:56:30 -0800

    up responsive testimonials

--- nexile-frontend/src/pages/LandingPage/LandingPage/Testimonials.jsx
+++ nexile-frontend/src/pages/LandingPage/LandingPage/Testimonials.jsx
@@ -52,7 +52,7 @@ const Testimonials = () => {
           What our clients are saying
         </h2>
         <div className="lg:w-[30%]">
-          <p className="font-[100] text-white text-[20px] lg:ml-[-200px]">
+          <p className="font-[100] text-white text-[20px] md:mt-0 mt-4 lg:ml-[-200px]">
             We have collaborated with or worked with them and helped them grow
             their business. With the help of Nexile Digital, you can also
             grow/boost your business.
@@ -68,7 +68,7 @@ const Testimonials = () => {
       </div>
 
       <div
-        className="h-[800px] w-full grid grid-cols-3 gap-10 justify-center items-center overflow-hidden"
+        className="h-[800px] w-full lg:grid lg:grid-cols-3 md:grid-cols-2 gap-10 justify-center items-center"
         style={{
           backgroundImage:
             "url(https://res.cloudinary.com/dnwmtd4p1/image/upload/v1734718037/nexile%20digital/asset/ow0kdpfdx5ebqmhyebxa.webp)",
@@ -78,11 +78,11 @@ const Testimonials = () => {
           zIndex: 0,
         }}
       >
-        <div className="col-span-1">
+        <div className="lg:col-span-1 col-span-1">
          <img src="https://res.cloudinary.com/dnwmtd4p1/image/upload/v1734735974/nexile%20digital/asset/k3gel9qz6m3wb5gxtanm.webp" alt="" />
         </div>
 
-        <div className="col-span-2">
+        <div className="lg:col-span-2 col-span-1">
         <Swiper
       spaceBetween={4}
       pagination={{ clickable: true }}
@@ -104,7 +104,7 @@ const Testimonials = () => {
           }`}
         >
           <div className="flex flex-col items-start p-6 rounded-lg">
-            <p className="mt-4 mb-10 z-20 text-white text-start text-[26px] poppins-regular">
+            <p className="mt-4 mb-10 z-20 text-white text-start md:text-[26px] text-[18px] poppins-regular">
               {expandedTestimonials[testimonial._id]
                 ? testimonial.testimonialText
                 : `${testimonial.testimonialText.slice(0, 164)}...`}
@@ -116,14 +116,14 @@ const Testimonials = () => {
               </p>
             </p>
             
-            <div className="flex gap-6">
-            <div className="w-24 h-24 relative mb-3">
+            <div className="flex md:flex-row flex-col justify-center items-center gap-6">
+            <div className="md:w-24 w-18 h-18 md:h-24 w-18 h-18 relative mb-3">
               <img
                 src={testimonial.clientImage}
                 alt={testimonial.clientName}
-                className="w-24 h-24 rounded-full shadow-lg"
+                className="md:w-24 w-18 h-18 md:h-24 w-18 h-18 rounded-full shadow-lg"
               />
-              <div className="bg-[#699396] w-8 h-8 rounded-full absolute right-0 bottom-0">
+              <div className="bg-[#699396] md:w-8 w-12 md:h-8 h-12 rounded-full absolute md:right-0 right-0 bottom-0">
                 <img
                   src="https://res.cloudinary.com/dnwmtd4p1/image/upload/v1734735095/nexile%20digital/asset/pueqdqaepb88a688mlqz.webp"
                   alt=""
@@ -131,17 +131,17 @@ const Testimonials = () => {
               </div>
             </div>
             <div>
-            <h3 className="mt-4 text-[25px] poppins-semibold text-white">
+            <h3 className="md:mt-4 mt-1 md:text-[25px] text-[20px] md:text-start text-center poppins-semibold text-white">
               {testimonial.clientName}
             </h3>
-            <p className="text-[20px]  text-white poppins-extralight underline">
+            <p className="md:text-[20px] text-[16px] text-white md:text-start text-center poppins-extralight underline">
               {testimonial.clientDescription}
             </p>
             </div>
           </div>
             </div>
             
-            <div className="flex items-center gap-3 ml-5">
+            <div className="flex items-center md:justify-start justify-center gap-3 ml-5">
             <div>
               <p className="text-[18px] text-white poppins-extralight">Reviews on</p>
               <img src="https://res.cloudinary.com/dnwmtd4p1/image/upload/v1731042430/nexile%20digital/asset/zyg5fsrl4n7oyssnvf06.webp"
